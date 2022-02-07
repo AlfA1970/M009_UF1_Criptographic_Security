8aecf004673681ff7e33d524b80d1b9343dadeff
Alfa1970 <alfa1970@pop-os.localdomain>
2022-02-07 19:48:19 +0100

    implemented encrypt function --don't work yet--

--- src/AES/Main.java
+++ src/AES/Main.java
@@ -1,22 +1,27 @@
 package AES;
 
+import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import javax.swing.*;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class Main {
 
     String KEY = "criptographicSecurity";
 
     public static void main(String[] args) {
+        Main main = new Main();
+        JOptionPane.showMessageDialog(null,main.Encrypt("hello"));
 
 
     }
 
     // Encrypt / Decrypt Class
 
-    public SecretKeySpec createKey(String key){
+    public SecretKeySpec CreateKey(String key){
         try{
             byte[]string = key.getBytes(StandardCharsets.UTF_8);
             MessageDigest md = MessageDigest.getInstance("SHA-1");
@@ -33,6 +38,18 @@ public class Main {
     // Encryptation method
 
     public String Encrypt(String encrypt){
+        try{
+            SecretKeySpec ske = CreateKey(KEY);
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE,ske);
+            byte[]string = encrypt.getBytes(StandardCharsets.UTF_8);
+            byte[]encrypted = cipher.doFinal(string);
+            String encruptedString = Base64.getEncoder().encodeToString(encrypted);
+            return encruptedString;
+
+        }catch (Exception e){
+            return null;
+        }
 
     }
 
