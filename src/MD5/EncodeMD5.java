a2e4db3666e4e60f7f524221cb62bfb56b69f218
Alfa1970 <alfa1970@pop-os.localdomain>
2022-02-08 20:34:14 +0100

    implemented MD5 ENCRYPT & DECRYPT -- is running yet --

--- src/MD5/EncodeMD5.java
+++ src/MD5/EncodeMD5.java
@@ -3,6 +3,7 @@ package MD5;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.swing.*;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Arrays;
@@ -10,40 +11,61 @@ import java.util.Base64;
 
 public class EncodeMD5 {
 
-    String secretKey="WeAreProgrammers!";
+    public String secretKey = "WeAreProgrammers!";
 
     public static void main(String[] args) {
+        EncodeMD5 md5 = new EncodeMD5();
+        String messageToEncrypt = JOptionPane.showInputDialog("Enter message to encrypt: ");
+        String stringEncrypted = md5.encode(md5.secretKey, messageToEncrypt);
+        JOptionPane.showMessageDialog(null, "String Encrypted: " + stringEncrypted);
+        String stringDecrypted = md5.decode(md5.secretKey, stringEncrypted);
+        JOptionPane.showMessageDialog(null, "String Decrypted: " + stringDecrypted);
 
     }
 
-    public String encode(String key, String stringToEncrypt){
-        String encryptation;
+    public String encode(String key, String stringToEncrypt) {
+        String encryptation = "";
 
-        try{
+        try {
             MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[]passkey = md.digest(secretKey.getBytes(StandardCharsets.UTF_8));
-            byte[]byteKey = Arrays.copyOf(passkey, 24);
-            SecretKey skey = new SecretKeySpec(byteKey,"DESed");
-            Cipher cipher = Cipher.getInstance("DESed");
-            cipher.init(Cipher.ENCRYPT_MODE,skey);
-            byte[]plainTextBytes =  stringToEncrypt.getBytes(StandardCharsets.UTF_8);
-            byte[]buf = cipher.doFinal(plainTextBytes);
-            byte[]stringB64 = Base64.getEncoder().encode(buf);
+            byte[] passkey = md.digest(secretKey.getBytes(StandardCharsets.UTF_8));
+            byte[] byteKey = Arrays.copyOf(passkey, 24);
+            SecretKey skey = new SecretKeySpec(byteKey, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, skey);
+            byte[] plainTextBytes = stringToEncrypt.getBytes(StandardCharsets.UTF_8);
+            byte[] buf = cipher.doFinal(plainTextBytes);
+            byte[] stringB64 = Base64.getEncoder().encode(buf);
             encryptation = new String(stringB64);
 
-            return encryptation;
-
-        }catch (Exception e){
+        } catch (Exception e) {
+            e.printStackTrace();
             System.out.println("Something went wrong!");
         }
 
-        return null;
-
+        return encryptation;
     }
 
-    public String decode(String key, String stringToEncrypt){
+    public String decode(String key, String stringToEncrypt) {
+        String decryptation = "";
 
-        return null;
+        try{
+            byte[]stringEncrypt = Base64.getDecoder().decode(stringToEncrypt.getBytes(StandardCharsets.UTF_8));
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] passkeyEncoded = md.digest(secretKey.getBytes(StandardCharsets.UTF_8));
+            byte[] byteKey = Arrays.copyOf(passkeyEncoded, 24);
+            SecretKey skey = new SecretKeySpec(byteKey, "AES");
+            Cipher decipher = Cipher.getInstance("AES");
+            decipher.init(Cipher.DECRYPT_MODE, skey);
+            byte[] plainTextBytes = decipher.doFinal(stringEncrypt);
+            decryptation = new String(plainTextBytes, StandardCharsets.UTF_8);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Something went wrong!");
+        }
+
+        return decryptation;
 
     }
 }
